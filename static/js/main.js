a82f5d18578f39b368f4a22c1e11087fa7822366
Super Zombi <rost201541@gmail.com>
2023-09-23 16:25:23 +0300

    добафил вормочку вагина

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,47 @@
+function login(){
+	let form_action = document.querySelector("#login-area input[name='form_action']:checked").value
+	let username = document.querySelector("#login-area input[name='username']").value.trim()
+	let password = document.querySelector("#login-area input[name='password']").value.trim()
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", `/${form_action}`)
+	xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	xhr.onload = _=>{
+		if (xhr.status == 200){ 
+			let answer = JSON.parse(xhr.response);
+			if (answer.successfully){
+				document.querySelector("#login-area").classList.add("hide")
+				document.querySelector("#game-searcher").classList.remove("hide")
+				document.querySelector("#game-searcher #username").value = username
+			}
+		} else{
+			let input = document.querySelector(`#login-area input[name='${answer.target}']`)
+			if (input){
+				input.setCustomValidity(answer.reason);
+				input.reportValidity();
+				input.onkeydown = _=> input.setCustomValidity('');
+			}
+			else{
+				alert(answer.reason)
+			}
+		}
+	}
+	xhr.send(JSON.stringify({'username': username, 'password': password}))
+}
+
+
 var timer;
 var socket;
 var ROOM_ID;
 var gameFounded = false;
 function search_game(){
-	let username = document.querySelector("#username").value.trim();
+	let username = document.querySelector("#game-searcher #username").value.trim();
 	if (username){
 		document.querySelector("#search_animation").classList.remove("hidden")
-		document.querySelector("#username").readOnly = true;
 		timer = searchTimer()
 
 		document.querySelector("#game-searcher").onsubmit = _=>{
 			document.querySelector("#search_animation").classList.add("hidden")
-			document.querySelector("#username").readOnly = false;
 			clearTimeout(timer)
 			document.querySelector("#game-searcher").onsubmit = _=>{search_game();return false;}
 			document.querySelector("#game-searcher [type=submit]").innerHTML = "Search Game"
